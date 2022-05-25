abd710958a269389ddefe869f20cd9a1b5b3c999
Harvysingh <105930231+Harvysingh@users.noreply.github.com>
2022-05-25 18:04:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
             tracer.WithService("test-go"),
             tracer.WithServiceVersion("h123"),
 	    tracer.WithUDS("/var/run/datadog/apm.socket"),
-            // tracer.WithAgentAddr("localhost:8126"),	
+            tracer.WithAgentAddr("localhost:8126"),	
         )
 
         defer tracer.Stop() 	
